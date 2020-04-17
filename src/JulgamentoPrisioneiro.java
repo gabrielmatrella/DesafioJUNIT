6b2811e6639dfc8c6b13d57b6db38a1ab63e1ba9
unknown <matrella2@gmail.com>
2020-04-17 10:49:39 -0300

    Realizado primeiro teste com erro

---
+++ src/JulgamentoPrisioneiro.java
@@ -0,0 +1,40 @@
+public class JulgamentoPrisioneiro {
+
+	private int PENA_INOCENCIA = 13;
+
+	private int PENA_CONDENACAO_MUTUA = 15;
+
+	private int PENA_CONDENACAO_INDIVIDUAL = 10;
+
+	private int PENA_CONDENACAO_CUMPLICES = 11;
+
+	public int calculaPena(Resposta respostaPrisioneiroA, Resposta respostaPrisioneiroB) {
+
+		if (respostaPrisioneiroA == Resposta.DELACAO) {
+
+			if (respostaPrisioneiroB == Resposta.DELACAO) {
+
+				return PENA_CONDENACAO_MUTUA;
+
+			} else {
+
+				return PENA_INOCENCIA;
+
+			}
+		} else {
+
+			if (respostaPrisioneiroB == Resposta.DELACAO) {
+
+				return PENA_CONDENACAO_INDIVIDUAL;
+
+			} else {
+
+				return PENA_CONDENACAO_CUMPLICES;
+
+			}
+
+		}
+
+	}
+
+}
--- test/JulgamentoPrisioneiroTeste.java
+++ test/JulgamentoPrisioneiroTeste.java
@@ -1,10 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class JulgamentoPrisioneiroTeste {
 	
 	@Test
-	public void testeJulgamentoPrisioneiro() {
+	public void testeCalculoPenaInocencia() {
+		Resposta respostaPrisioneiroA = Resposta.DELACAO;
+		Resposta respostaPrisoneiroB = Resposta.SEM_DELACAO;
 		
+		JulgamentoPrisioneiro julgamentoPrisioneiro = new JulgamentoPrisioneiro();
+		int penaCalculada = julgamentoPrisioneiro.calculaPena(respostaPrisioneiroA, respostaPrisoneiroB);
+		
+		assertEquals(13, penaCalculada);
 	}
 	
 	
