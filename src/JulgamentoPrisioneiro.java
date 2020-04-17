e82784b548f2f8c5899875fcffb8163b48ac2bb1
unknown <matrella2@gmail.com>
2020-04-17 11:29:59 -0300

    Teste com erro em alguns casos corrigido na classe

--- src/JulgamentoPrisioneiro.java
+++ src/JulgamentoPrisioneiro.java
@@ -3,34 +3,25 @@ public class JulgamentoPrisioneiro {
 	private static int PENA_INOCENCIA = 0;
 
 	private static int PENA_CONDENACAO_MUTUA = 5;
-	
+
 	private static int PENA_CONDENACAO_INDIVIDUAL = 10;
-	
+
 	private static int PENA_CONDENACAO_CUMPLICES = 1;
 
 	public int calculaPena(Resposta respostaPrisioneiroA, Resposta respostaPrisioneiroB) {
-
 		if (respostaPrisioneiroA == Resposta.DELACAO) {
 			if (respostaPrisioneiroB == Resposta.DELACAO) {
 				return PENA_CONDENACAO_MUTUA;
 			} else {
-				return PENA_INOCENCIA;
+				return PENA_CONDENACAO_INDIVIDUAL;
 			}
 		} else {
-
 			if (respostaPrisioneiroB == Resposta.DELACAO) {
-
-				return PENA_CONDENACAO_INDIVIDUAL;
-
-			} else {
-
 				return PENA_CONDENACAO_CUMPLICES;
-
+			} else {
+				return PENA_INOCENCIA;
 			}
-
 		}
-
 	}
-
 
 }
